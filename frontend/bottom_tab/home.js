41f8aed05ebb5c3cf1cbde4f7097f79c5ecd93e7
Saumya Sanghvi <saumyasanghvi25@gmail.com>
2023-04-02 10:02:06 -0700

    Added image, change image orientations

--- frontend/bottom_tab/home.js
+++ frontend/bottom_tab/home.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function Home({navigation, route}) {
   
   return (
     <View style={styles.container}>
+    <Image
+        source={require('../assets/roadmap.png')}
+
+    />
       <Text>Home Tab</Text>
       <StatusBar style="auto" />
     </View>
