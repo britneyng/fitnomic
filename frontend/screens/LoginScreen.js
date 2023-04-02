6d0107cbcaa4112c3c6281ba8b94856c5df239c5
Saumya Sanghvi <saumyasanghvi25@gmail.com>
2023-04-01 19:27:05 -0700

    Added boilerplate login and home screen

---
+++ frontend/screens/LoginScreen.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, View, Text } from 'react-native';
+
+export default function LoginScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text>Email:</Text> 
+      <Text>Password:</Text>
+      <Button
+        title="Sign up"
+        onPress={() => navigation.navigate("Home", {none: "none"})}
+        />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#968CE1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
