9eb88edd2b6c8c0a28d9449a23519b7fafcc9868
Saumya Sanghvi <saumyasanghvi25@gmail.com>
2023-04-02 02:39:30 -0700

    removed comments

--- frontend/screens/EventsScreen.js
+++ frontend/screens/EventsScreen.js
@@ -10,11 +10,5 @@ export default function EventsScreen({navigation, route}) {
       <Text>Events Screen</Text>
       <StatusBar style="auto" />
     </View>
-    // <Tab.Navigator>
-    //     <Tab.Screen name="Home" component={home} />
-    //     <Tab.Screen name="Events" component={events} />
-    //     <Tab.Screen name="Rewards" component={rewards} />
-    //     <Tab.Screen name="Profile" component={profile} />
-    // </Tab.Navigator>
   );
 }
